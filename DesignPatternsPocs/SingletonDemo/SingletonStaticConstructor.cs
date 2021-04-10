d26c81ef8982af20289edbbee9b697936d66c275
BasmAshraf <basmashrafarag@gmail.com>
2021-04-10 11:19:49 +0200

    add singleton static constructor approach

---
+++ DesignPatternsPocs/SingletonDemo/SingletonStaticConstructor.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SingletonDemo.StaticConstructorApproach
+{
+    public class Singleton
+    {
+        #region Static Constructor version1
+        //private static readonly Singleton _instanace = new Singleton();
+        //public static string x = "hamada";
+        //private Singleton()
+        //{
+
+        //}
+
+        //static Singleton()
+        //{
+
+        //}
+
+        //public static Singleton Instance
+        //{
+        //    get
+        //    {
+        //        return _instanace;
+        //    }
+        //}
+        #endregion
+
+        #region Static Constructor version2 (Modified)
+        //To fix the issue of instatiating the instance from any static member so here we try to instantiate on demand as much as we can
+        public static string x = "hamada";
+        private Singleton()
+        {
+
+        }
+
+        static Singleton()
+        {
+
+        }
+
+        public static Singleton Instance
+        {
+            get
+            {
+                return Nested._instanace;
+            }
+        }
+        private class Nested
+        {
+            public static readonly Singleton _instanace = new Singleton();
+            static Nested()
+            {
+            }
+        }
+        #endregion
+    }
+}
---
+++ DesignPatternsPocs/Test/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using SingletonDemo.StaticConstructorApproach;
+
+namespace Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Test Singleton Static Constructor version1
+            var y = Singleton.x;
+            var x = Singleton.Instance;
+            var x1 = Singleton.Instance;
+            var x2 = Singleton.Instance;
+            #endregion
+
+        }
+    }
+}
