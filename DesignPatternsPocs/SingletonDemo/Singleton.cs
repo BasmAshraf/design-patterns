7630b8bda7e16af6c950b5fd421ce6300f9ad3be
BasmAshraf <basmashrafarag@gmail.com>
2021-04-10 11:59:44 +0200

    using Lazt<T> to implement singleton

--- DesignPatternsPocs/SingletonDemo/Singleton.cs
+++ DesignPatternsPocs/SingletonDemo/Singleton.cs
@@ -5,10 +5,47 @@ namespace SingletonDemo.NaiveApproach
 {
     //NaiveApproach, not thread-safe
     //Adding lock, thread-safe
+    //Enhace using Lazy<T> without locking it is thread safe by default
     public class Singleton
     {
-        private static Singleton _instance;
-        private static object padLock = new object();
+        #region Singleton Naive approach with locking
+        //private static Singleton _instance;
+        //private static object padLock = new object();
+        //private Singleton()
+        //{
+        //    Logger.Log("Constructor invoked");
+        //}
+
+        //public static Singleton Instance
+        //{
+        //    get
+        //    {
+        //        Logger.Log("Instance called");
+        //        //lock (padLock)
+        //        //{
+        //        //    if (_instance == null)
+        //        //        _instance = new Singleton();
+        //        //    return _instance;
+        //        //}
+
+        //        //double checking lock approach
+        //        if (_instance == null)
+        //        {
+        //            lock (padLock)
+        //            {
+        //                if (_instance == null)
+        //                {
+        //                    _instance = new Singleton();
+        //                }
+        //            }
+        //        }
+        //        return _instance;
+        //    }
+        //}
+        #endregion
+
+        #region Singleton using Lazy<T>
+        private static Lazy<Singleton> _instance= new Lazy<Singleton>(()=>new Singleton());
         private Singleton()
         {
             Logger.Log("Constructor invoked");
@@ -19,31 +56,15 @@ namespace SingletonDemo.NaiveApproach
             get
             {
                 Logger.Log("Instance called");
-                //lock (padLock)
-                //{
-                //    if (_instance == null)
-                //        _instance = new Singleton();
-                //    return _instance;
-                //}
-
-                //double checking lock approach
-                if (_instance == null)
-                {
-                    lock (padLock)
-                    {
-                        if (_instance == null)
-                        {
-                            _instance = new Singleton();
-                        }
-                    }
-                }
-                return _instance;
+                return _instance.Value;
             }
         }
 
+        #endregion
         public static void Reset()
         {
-            _instance = null;
+            if(_instance is Singleton)
+                 _instance = null;
         }
     }
 }
