9928a57b667fc60a42e61a88c171e8c014bde948
BasmAshraf <basmashrafarag@gmail.com>
2021-04-11 23:33:31 +0200

    init command pattern demo

---
+++ DesignPatternsPocs/ShoppingCart.Business/Repositories/Interfaces/IProductsRepository.cs
@@ -0,0 +1,16 @@
+﻿using ShoppingCart.Business.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ShoppingCart.Business.Repositories.Interfaces
+{
+    public interface IProductRepository
+    {
+        Product FindBy(string articleId);
+        int GetStockFor(string articleId);
+        IEnumerable<Product> All();
+        void DecreaseStockBy(string articleId, int amount);
+        void IncreaseStockBy(string articleId, int amount);
+    }
+}
---
+++ DesignPatternsPocs/ShoppingCart.Business/Repositories/Interfaces/IShoppingCartRepository.cs
@@ -0,0 +1,17 @@
+﻿using ShoppingCart.Business.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ShoppingCart.Business.Repositories.Interfaces
+{
+    public interface IShoppingCartRepository
+    {
+        (Product Product, int Quantity) Get(string articleId);
+        IEnumerable<(Product Product, int Quantity)> All();
+        void Add(Product product);
+        void RemoveAll(string articleId);
+        void IncreaseQuantity(string articleId);
+        void DecraseQuantity(string articleId);
+    }
+}
