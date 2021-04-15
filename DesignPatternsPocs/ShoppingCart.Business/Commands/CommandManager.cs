5514f63c81cd1f67ab5cdb35e4157d03a0e25903
BasmAshraf <basmashrafarag@gmail.com>
2021-04-15 15:19:19 +0200

    add command interfacr and command manager

---
+++ DesignPatternsPocs/ShoppingCart.Business/Commands/CommandManager.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ShoppingCart.Business.Commands
+{
+    public class CommandManager
+    {
+        private Stack<ICommand> _commands = new Stack<ICommand>();
+
+        public void Invoke(ICommand command)
+        {
+            if (command.CanExecute())
+            {
+                command.Execute();
+                _commands.Push(command);
+            }
+        }
+
+        public void Undo()
+        {
+            while(_commands.Count>0)
+            {
+                var command = _commands.Pop();
+                command.Undo();
+            }
+        }
+
+    }
+}
