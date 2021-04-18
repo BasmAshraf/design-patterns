a44569d1e953d9e9e44da5c6727241ec5443006d
BasmAshraf <basmashrafarag@gmail.com>
2021-04-18 09:52:26 +0200

    test commands using console app

--- DesignPatternsPocs/ShoppingCart/Program.cs
+++ DesignPatternsPocs/ShoppingCart/Program.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using ShoppingCart.Business.Commands;
+using ShoppingCart.Business.Constants;
+using ShoppingCart.Business.Repositories;
+using System;
 
 namespace ShoppingCart
 {
@@ -6,7 +9,50 @@ namespace ShoppingCart
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var shoppingCartRepository = new ShoppingCartRepository();
+            var productsRepository = new ProductsRepository();
+
+            var product = productsRepository.FindBy("SM7B");
+
+            var addToCartCommand = new AddToCartCommand(
+                productsRepository,
+                shoppingCartRepository,
+                product);
+
+            var increaseQuantityCommand = new ChangeQuantityCommand(
+                productsRepository,
+                shoppingCartRepository,
+                product,
+                Operation.Increase);
+
+            var manager = new CommandManager();
+            manager.Invoke(addToCartCommand);
+            manager.Invoke(increaseQuantityCommand);
+            manager.Invoke(increaseQuantityCommand);
+            manager.Invoke(increaseQuantityCommand);
+            manager.Invoke(increaseQuantityCommand);
+
+            PrintCart(shoppingCartRepository);
+
+            manager.Undo();
+
+            PrintCart(shoppingCartRepository);
+        }
+
+        static void PrintCart(ShoppingCartRepository shoppingCartRepository)
+        {
+            var totalPrice = 0m;
+            foreach (var lineItem in shoppingCartRepository.LineItems)
+            {
+                var price = lineItem.Value.Product.Price * lineItem.Value.Quantity;
+
+                Console.WriteLine($"{lineItem.Key} " +
+                    $"${lineItem.Value.Product.Price} x {lineItem.Value.Quantity} = ${price}");
+
+                totalPrice += price;
+            }
+
+            Console.WriteLine($"Total price:\t${totalPrice}");
         }
     }
 }
