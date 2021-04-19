d2c786fc969d78c4992d18e71351b46b972e8e79
BasmAshraf <basmashrafarag@gmail.com>
2021-04-19 10:03:21 +0200

    init null object pattern demo

---
+++ DesignPatternsPocs/NullObjectPattern/Program.cs
@@ -0,0 +1,19 @@
+﻿using NullObjectPattern.Entities;
+using NullObjectPattern.Services;
+using NullObjectPattern.View;
+using System;
+
+namespace NullObjectPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LearnerService learnerService = new LearnerService();
+            ILearner learner = learnerService.GetCurrentLearner();
+
+            LearnerView view = new LearnerView(learner);
+            view.RenderView();
+        }
+    }
+}
