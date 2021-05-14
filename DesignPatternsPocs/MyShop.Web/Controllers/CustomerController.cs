ab99d5fb8c6cc328d7c608fdf648ee3f215213bc
BasmAshraf <basmashrafarag@gmail.com>
2021-05-14 14:14:55 +0200

    adding and consuming repository layer

--- DesignPatternsPocs/MyShop.Web/Controllers/CustomerController.cs
+++ DesignPatternsPocs/MyShop.Web/Controllers/CustomerController.cs
@@ -1,30 +1,30 @@
 ﻿using System;
-using System.Linq;
 using Microsoft.AspNetCore.Mvc;
-using MyShop.Infrastructure;
+using MyShop.Domain.Models;
+using MyShop.Infrastructure.Repositories;
 
 namespace MyShop.Web.Controllers
 {
     public class CustomerController : Controller
     {
-        private ShoppingContext context;
+        private readonly IRepository<Customer> repository;
 
-        public CustomerController()
+        public CustomerController(IRepository<Customer> repository)
         {
-            context = new ShoppingContext();
+            this.repository = repository;
         }
 
         public IActionResult Index(Guid? id)
         {
             if (id == null)
             {
-                var customers = context.Customers.ToList();
+                var customers = repository.All();
 
                 return View(customers);
             }
             else
             {
-                var customer = context.Customers.Find(id.Value);
+                var customer = repository.Get(id.Value);
 
                 return View(new[] { customer });
             }
